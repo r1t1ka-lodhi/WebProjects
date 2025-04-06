fd8e39886747084cde930fc2a848df3cfd5f8c28
Ritika Lodhi <ritikalodhi3@gmail.com>
2025-04-06 15:56:58 +0530

    Add files via upload

---
+++ simpletabs/tabs.js
@@ -0,0 +1,15 @@
+
+document.addEventListener("DOMContentLoaded",()=>{
+    const tabs=document.querySelectorAll(".tab");
+    const contents=document.querySelectorAll(".tab-content");
+
+    tabs.forEach(tab=>{
+        tab.addEventListener("click",()=>{
+            tabs.forEach(t=>t.classList.remove("active"));
+            contents.forEach(c=>c.classList.remove("active"));
+
+            tab.classList.add("active");
+            document.getElementById(tab.dataset.tab).classList.add("active");
+        });
+    });
+});
